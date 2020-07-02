1d7762de8d739c731bd031e8592df0570d94e2db
Rashid ALi <49899864+rashid327@users.noreply.github.com>
2020-07-02 13:27:33 -0700

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -62,13 +62,12 @@ $(function() {
       return $(this).is(":visible");
     },
   });
-
+\
   $("a[data-toggle=\"tab\"]").click(function(e) {
     e.preventDefault();
     $(this).tab("show");
   });
 });
-
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
   $('#success').html('');
